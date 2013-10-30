349427952e2b921dc4ffc4306907b77266094126
Carl-Erik Kopseng <carlerik@gmail.com>
2013-10-31 00:32:56 +0100

    Completed new function SensorReading.findAllOnDate()
    Can new retrieve all sensor readings for a given date.

--- js/models/SensorReading.js
+++ js/models/SensorReading.js
@@ -1,7 +1,6 @@
 /* global: can app */
 (function () {
 	'use strict';
-	var url = '/plants/{id}/sensorvalues.json';
 //	var url = '/plants/{id}/sensorvalues.json?from={from}&to={to}&page={page}';
 //	var url = '/plants/1/sensorvalues.json?&to=2013-10-26&from=2013-10-24&page=10';
 
@@ -13,8 +12,7 @@
 		var s = url;
 		for (var token in tokens) {
 			if (tokens.hasOwnProperty(token)) {
-				console.log(token, tokens)
-				s = s.replace(new RegExp('{' + token + '}'), tokens[token]);
+				s = s.replace(new RegExp('{' + token + '}', 'g'), tokens[token]);
 			}
 		}
 		return s;
@@ -35,13 +33,85 @@
 		return new app.SensorReading(data[data.length - 1]);
 	}
 
+	function getLinkHeader (jqXHR) {
+		var responseHeader = jqXHR.getResponseHeader("Link");
+		if (!responseHeader) {
+			throw new Error("Server did not return Link header.");
+		}
+		return responseHeader;
+	}
+
 	app.SensorReading = can.Model.extend({
-		findOne          : 'GET /plants/{id}/sensorvalues.json?from={from}&page=1',
-		findAll          : 'GET ' + url,
+		/**
+		 * Meant to be used to just get the first data object
+		 * @returns an array of data from the given date
+		 */
+		findOne : 'GET /plants/{id}/sensorvalues.json?from={from}&page=1',
+
+		/**
+		 * Retrieve all sensor data from a given date
+		 * Will resolve the deferred almost immediately, but will continue to fill the resolved observable list with data
+		 * The resolved list will be empty if no data could be found for that date
+		 *
+		 * @param params.id {String}
+		 * @param params.date {String} an iso date
+		 *
+		 * @returns {Deferred}
+		 */
+		findAllOnDate : function (params) {
+			var dfd = $.Deferred(),
+				resultList = app.SensorReading.List(),
+				url = '/plants/{id}/sensorvalues.json?from={date}&to={date}&page={page}',
+				oneDayInMillis = 1000 * 3600 * 24,
+				nextDayMillis = new Date(params.from).getTime() + oneDayInMillis;
+
+			function appendToList (rawReadings) {
+				rawReadings.forEach(function (reading) {
+					resultList.push(new app.SensorReading(reading));
+				});
+			}
+
+			$.ajax(replaceTokens(url, $.extend({}, params, { page : 1})))
+				.then(function (data, textStatus, jqXHR) {
+					var responseHeader, parser, dfd, lastUrl;
+
+					dfd = $.Deferred();
+
+					if (data.length === 0) { dfd.reject(); }
+					else {
+						appendToList(data);
+
+						responseHeader = getLinkHeader(jqXHR);
+						parser = new app.LinkHeaderParser(responseHeader);
+						lastUrl = parser.getUrlOfLastResultPage();
+
+						var match = lastUrl.match(/page=(\d+)/), numberOfPages;
+						if (!match) { throw new Error('Could not parse url: ' + lastUrl); }
+
+						numberOfPages = match[1];
+						dfd.resolve(numberOfPages);
+					}
+					return dfd;
+				})
+				.then(function (numberOfPages) {
+					for (var i = 2; i <= numberOfPages; i++) {
+						$.ajax(replaceTokens(url, $.extend({}, params, { page : i}))).done(appendToList);
+					}
+				});
+
+			// Resolve immediately, but since the list is an observable, we can keep on adding to it!
+			dfd.resolve(resultList);
+
+			return dfd.promise();
+		},
+
+		/** Get the first and last sensor reading - given the params */
 		findFirstAndLast : function (params) {
 			if (!this.cache) { this.cache = {}; }
 
-			var dfd = $.Deferred(), cache = this.cache;
+			var dfd = $.Deferred(),
+				cache = this.cache,
+				url = '/plants/{id}/sensorvalues.json?page=1';
 
 
 			if (cache[params.id]) {
@@ -49,8 +119,8 @@
 				return dfd;
 			}
 
-			$.ajax(replaceTokens(url, params), {
-				success : function (data, textStatus, jqXHR) {
+			$.ajax(replaceTokens(url, params))
+				.done(function (data, textStatus, jqXHR) {
 					var responseHeader , h, lastUrl, first;
 
 					function buildResultAndResolve (data) {
@@ -59,14 +129,11 @@
 						dfd.resolve(result);
 					}
 
-					first = firstReading(data);
-					responseHeader = jqXHR.getResponseHeader("Link");
-					if (!responseHeader) {
-						dfd.reject();
-						throw new Error("Server did not return Link header.");
-					}
+					responseHeader = getLinkHeader(jqXHR);
 
 					h = new app.LinkHeaderParser(responseHeader);
+
+					first = firstReading(data);
 					lastUrl = stripHostName(h.getUrlOfLastResultPage());
 
 					// if none found, means all results are contained on the current page
@@ -76,10 +143,9 @@
 						// go the the last result on the server and fetch that page
 						$.ajax(lastUrl).done(buildResultAndResolve);
 					}
-				}
-			});
+				});
 
-			return dfd;
+			return dfd.promise();
 		}
 	}, {
 		/* utility functions */
